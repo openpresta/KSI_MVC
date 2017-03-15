649eda762b7db0b84c1f8afda3223a45be7bc424
twizyx <info@twizyx.com>
2017-03-15 13:18:55 +0100

    Externalisation du mobileMenu

---
+++ views/mobileMenu.php
@@ -0,0 +1,55 @@
+<!-- Mobile Menu //-->
+		<div class="ct-menu-mobile">
+			<nav class="navbar navbar-dark">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-4 col-sm-4 hidden-xs">
+							<ul class="list-unstyled list-inline ct-socials--rounded">
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+							</ul>
+						</div>
+						<div class="col-xs-7 col-sm-4">
+							<div class="navbar-header"><a href="index.html" class="navbar-brand"><img style="height: 30px; padding-top: 12px;" src="<?php echo PATH_PICS; ?>logo.png" alt=""></a></div>
+						</div>
+						<div class="col-xs-5 col-sm-4">
+							<ul role="menu" class="nav navbar-nav">
+								<li class="ct-productCart-link"><a><i class="fa fa-shopping-cart"></i></a>
+								</li>
+								<li class="nav-item-toggle"><a><i class="fa fa-bars"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</nav>
+		</div>
+		<div class="navbar-beacon">
+			<ul class="list-unstyled">
+				<li class="nav-item dropdown">
+					<a href="#">Boîtier additionnel<i class="fa fa-angle-double-down"></i></a>
+					<ul role="menu" class="dropdown-menu">
+						<li class="nav-item"><a href="index3.html">Présentation</a></li>
+						<li class="nav-item"><a href="index.html">Atouts</a></li>
+						<li class="nav-item"><a href="index5.html">Certifications</a></li>
+						<li class="nav-item"><a href="index2.html">Technologies</a></li>
+					</ul>
+				</li>
+				<li class="nav-item dropdown">
+					<a href="#">Module accélérateur<i class="fa fa-angle-double-down"></i></a>
+					<ul role="menu" class="dropdown-menu">
+						<li class="nav-item"><a href="index3.html">Présentation</a></li>
+						<li class="nav-item"><a href="index.html">Atouts</a></li>
+						<li class="nav-item"><a href="index5.html">Certifications</a></li>
+						<li class="nav-item"><a href="index2.html">Technologies</a></li>
+					</ul>
+				</li>
+				<li class="nav-item"><a href="#features" data-scroll="#features">FAQ</a></li>
+				<li class="nav-item"><a href="#product" data-scroll="#product">Contact</a></li>
+			</ul>
+			<ul class="list-unstyled list-inline ct-socials--rounded">
+				<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+				<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+			</ul>
+		</div>
