c3762a7883acdea0605e647cdf6af282e58e58d7
twizyx <info@twizyx.com>
2017-03-16 12:39:45 +0100

    Inclusion du menu desktop (navbar)

--- views/pedalbox.php
+++ views/pedalbox.php
@@ -38,25 +38,9 @@
 			</div>
 		</div>
 		<div id="ct-js-wrapper" class="ct-js-wrapper ct-pageWrapper">
-			<!-- Navigation-->
-			<nav data-height="80" class="navbar navbar-default navbar-light navbar-desktop navbar--animated navbar-top">
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-12">
-							<ul class="navbar-buttons list-unstyled list-inline pull-right">
-								<li><a href="#order" data-scroll="#order" class="btn-group btn-group--separated"><span class="btn btn-xs btn-transparent btn--white btn-separated">mon véhicule</span><span class="btn btn-xs btn-transparent btn--white btn-separated"><i class="fa fa-search"></i></span></a></li>
-							</ul>
-							<ul role="menu" class="nav navbar-nav ct-navbar--fadeIn">
-								<li class="nav-item"><a href="#features" data-scroll="#features">Accueil</a></li>
-								<li class="nav-item"><a href="#features" data-scroll="#features">Boîtier additionnel</a></li>
-								<li class="nav-item"><a href="#product" data-scroll="#product">Module accélerateur</a></li>
-								<li class="nav-item"><a href="about-us.html">FAQ</a></li>
-								<li class="nav-item"><a href="contact-us.html">Contact</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</nav>
+			
+			<?php include(PATH_VIEWS . 'navbar.php'); ?>
+			
 			<header data-background="<?php echo PATH_PICS; ?>rs6_gray.jpg" data-height="60%" class="ct-header darken">
 				<div class="inner">
 					<div class="container">
--- views/vehicle.php
+++ views/vehicle.php
@@ -38,26 +38,9 @@
 			</div>
 		</div>
 		<div id="ct-js-wrapper" class="ct-js-wrapper ct-pageWrapper">
-			<!-- Navigation-->
-			<nav data-height="80" class="navbar navbar-default navbar-light navbar-desktop navbar--animated navbar-top">
-				<div class="container">
-					<div class="row">
-						<div class="col-sm-12">
-							<div class="navbar-header"><a href="index.html" class="navbar-brand"><img src="<?php echo PATH_PICS; ?>logo_500px.png" alt="" style="height: 40px; padding: 5px;"></a></div>
-							<ul class="navbar-buttons list-unstyled list-inline pull-right">
-								<li><a href="#order" data-scroll="#order" class="btn-group btn-group--separated"><span class="btn btn-xs btn-transparent btn--white btn-separated">mon véhicule</span><span class="btn btn-xs btn-transparent btn--white btn-separated"><i class="fa fa-search"></i></span></a></li>
-							</ul>
-							<ul role="menu" class="nav navbar-nav ct-navbar--fadeIn">
-								<li class="nav-item"><a href="#features" data-scroll="#features">Accueil</a></li>
-								<li class="nav-item"><a href="#features" data-scroll="#features">Boîtier additionnel</a></li>
-								<li class="nav-item"><a href="#product" data-scroll="#product">Module accélerateur</a></li>
-								<li class="nav-item"><a href="about-us.html">FAQ</a></li>
-								<li class="nav-item"><a href="contact-us.html">Contact</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</nav>
+			
+			<?php include(PATH_VIEWS . 'navbar.php'); ?>
+			
 			<header data-background="<?php echo PATH_MAKES_COVER . $make_url . ".jpg" ?>" data-height="50%" class="ct-header darken">
 				<div class="inner">
 					<div class="container">
